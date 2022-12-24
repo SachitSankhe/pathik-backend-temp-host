31a0c9560dd87e09e4bdc691c975cccfde024849
Sachit_Sankhe <sachitsankhe@gmail.com>
2022-12-24 19:53:39 +0530

    some changes regarding model

--- payment/models.py
+++ payment/models.py
@@ -6,7 +6,7 @@ from tickets.models import Ticket
 
 
 class Payment(models.Model):
-    ID = models.UUIDField(default=uuid.uuid4, editable=False)
+    ID = models.CharField(default=uuid.uuid4, editable=False,max_length=500)
     payment_request_id = models.CharField(max_length=150)
     payment_id = models.CharField(max_length=150)
     ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
