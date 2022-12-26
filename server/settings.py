746d95afd088b06ee1af38a69ed82b70a186badd
Sachit_Sankhe <sachitsankhe@gmail.com>
2022-12-26 12:24:10 +0530

    404 page changed

--- server/settings.py
+++ server/settings.py
@@ -145,9 +145,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'server/static'),
+    os.path.join(BASE_DIR, 'server\static'),
 ]
 
 # Media Folder Settings
